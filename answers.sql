6589e2e2446e7702cb6a07858b9f6108c713adac
bxker <jake.luke.baker@gmail.com>
2019-09-11 15:40:34 -0500

    finished through group by

--- answers.sql
+++ answers.sql
@@ -69,3 +69,122 @@ INNER JOIN album AS al
 ON al.album_id = t.album_id
 INNER JOIN artist AS a
 ON a.artist_id = al.artist_id;
+
+
+--Practice nested Queries
+
+--1
+SELECT *
+FROM invoice 
+WHERE invoice_id
+IN 
+(SELECT invoice_id FROM invoice_line WHERE unit_price > 0.99);
+
+--2
+SELECT * 
+FROM playlist_track
+WHERE playlist_id
+IN
+(SELECT playlist_id FROM playlist WHERE name = 'Music');
+
+--3
+SELECT name 
+FROM track
+WHERE track_id
+IN
+(SELECT track_id FROM playlist_track WHERE playlist_id = 5);
+
+--4
+SELECT * 
+FROM track
+WHERE track_id
+IN
+(SELECT track_id FROM genre WHERE name = 'Comedy');
+
+--5
+SELECT * 
+FROM track
+WHERE track_id
+IN
+(SELECT track_id FROM album WHERE title = 'Fireball');
+
+--6
+SELECT * 
+FROM track
+WHERE album_id
+IN
+(SELECT album_id FROM album WHERE artist_id IN 
+ 	(SELECT artist_id from artist WHERE name = 'Queen'));
+
+--black diamond
+
+
+--Practice updating Rows
+
+--1
+UPDATE customer
+SET fax = NULL
+WHERE fax IS NOT NULL;
+
+--2
+UPDATE customer
+SET company = 'Self'
+WHERE company IS NULL;
+
+--3
+UPDATE customer
+SET last_name = 'Thompson'
+WHERE first_name = 'Julie' AND last_name = 'Barnett';
+
+--4
+UPDATE customer
+SET support_rep_id = 4
+WHERE email = 'luisrojas@yahoo.cl';
+
+--5
+UPDATE track
+SET composer = 'The darkness around us'
+WHERE genre_id = (SELECT genre_id FROM genre WHERE name = 'Metal')
+AND composer IS NULL;
+
+--6
+--refreshed
+
+--Group by
+
+--1
+SELECT COUNT(*), g.name
+FROM track t
+INNER JOIN genre g
+ON t.genre_id = g.genre_id
+GROUP BY g.name;
+
+--2
+SELECT COUNT(*), g.name
+FROM track t
+INNER JOIN genre g
+ON t.genre_id = g.genre_id
+WHERE g.name = 'Pop' OR g.name = 'Rock'
+GROUP BY g.name;
+
+--3
+SELECT COUNT(*), a.name
+FROM album al
+INNER JOIN artist a
+ON a.artist_id = al.artist_id
+GROUP BY a.name;
+
+
+--Use Distinct
+
+--1
+--2
+--3
+
+
+--Delete Rows
+
+--1
+--2
+--3
+
